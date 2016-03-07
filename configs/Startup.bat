fe52e8304e9f0e8eeb0e2bfd1257801c0c049ca5
Daniel Corbe <corbe@corbe.net>
2016-03-07 10:31:42 -0800

    Updated copy of startup.bat

--- configs/Startup.bat
+++ configs/Startup.bat
@@ -1,38 +1,38 @@
 @echo off
 
 timeout 5
-REM taskkill /f /fi "status eq not responding" /im arma3server.exe
+REM taskkill /f /fi "status eq not responding" /im armarma.exe
 taskkill /f /im arma3server.exe
 taskkill /im Bec.exe
 
 timeout 5
-if exist E:\update (
+if exist c:\update (
 	echo update folder present
-	cd E:\update
+	cd c:\update
 	if exist Altis_Life.Altis (
 		echo MAltis_Life.Altis folder is being updated
 		ren Altis_Life.Altis MAltis_Life.Altis	
-		rd /q /s E:\arma3server\MPMissions\MAltis_Life.Altis
-		move MAltis_Life.Altis E:\arma3server\MPMissions
+		rd /q /s c:\arma\MPMissions\MAltis_Life.Altis
+		move MAltis_Life.Altis c:\arma\MPMissions
 	)
 	if exist life_server.pbo (
 		rd /q /s life_server
 		echo life_server.pbo is being updated
-		move life_server.pbo E:\arma3server\@life_server\addons\life_server.pbo
+		move life_server.pbo c:\arma\@life_server\addons\life_server.pbo
 	)
 	if exist @extDB (
 		echo extDB is being updated
-		move @extDB\extdb\db_custom\altis-life-rpg-4.ini E:\arma3server\@extDB\extDB\db_custom
+		move @extDB\extdb\db_custom\altis-life-rpg-4.ini  c:\arma\@extDB\extDB\db_custom
 		rd /q /s @extDB
 	)
 )
 timeout 1
-cd E:\arma3server\
-start arma3server.exe "-config=E:\_arma3serverData\configs\serverConfig.cfg" "-cfg=E:\_arma3serverData\configs\server_network.cfg" "-BEPath=E:\_arma3serverData\battleye" "-profiles=E:\_arma3serverData\Profiles" "-mod=@life_server;@extDB" "-enableHT"
+cd c:\arma\
+start arma3server.exe "-config=c:\arma\configs\serverConfig.cfg" "-cfg=c:\arma\configs\server_network.cfg" "-BEPath=c:\arma\battleye" "-profiles=c:\arma\Profiles" "-mod=@life_server;@extDB;@infiSTAR_A3" "-enableHT " "-loadMissionToMemory" "-autoInit"
 echo ARMA 3 Server has started.
 
-timeout 10
-set becpath="E:\_arma3serverData\Bec"
+timeout 60
+set becpath="c:\arma\Bec"
 cd /d %becpath%
 start "" /min "Bec.exe" -f Config.cfg
 echo Battleye has started.. 
